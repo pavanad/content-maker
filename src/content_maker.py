c945b96cba6f10d2c4264ccd686a72a55114e88e
Adilson Pavan <adilson.pavan@gmail.com>
2019-10-26 17:42:26 -0300

    adding feature for ask search term and prefix

--- src/content_maker.py
+++ src/content_maker.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from collections import ask_search_term, ask_prefix
+
+def main():
+    search_term = ask_search_term()
+    prefix = ask_prefix()
 
 if __name__ == "__main__":
-    pass
+    main()
 
