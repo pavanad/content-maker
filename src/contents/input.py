518aebd46e419e0cb98f98b5a59fb3287889dee1
Adilson Pavan <adilson.pavan@gmail.com>
2019-11-08 00:13:26 -0300

    finish text collection and change autopep8 for balck

--- src/contents/input.py
+++ src/contents/input.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from pick import pick
 
-
-def ask_search_term(content):
+def ask_search_term(content: dict):
     content["search_term"] = input("Type a Wikipedia search term: ")
 
 
-def ask_prefix(content):
+def ask_prefix(content: dict):
     title = "Please choose on option: "
     options = ["Who is", "What is", "The history of"]
-    option, index = pick(options, title)
+    # option, index = pick(options, title)
     content["prefix"] = option
