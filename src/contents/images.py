e8334e35472a74fde59695b03da574a553d2fac2
Adilson Pavan <adilson.pavan@gmail.com>
2019-11-11 00:30:36 -0300

    querying google images and downloading

--- src/contents/images.py
+++ src/contents/images.py
@@ -2,6 +2,7 @@
 
 import os
 
+import requests
 from googleapiclient.discovery import build
 
 # google credentials
@@ -19,5 +20,35 @@ def fetch_images_from_google(content: dict):
 
 def fetch_images_all_sentences(content: dict):
     sentences = content["sentences"]
-    for sentence in sentences:
-        pass
+    for i, sentence in enumerate(sentences):
+        query = f"{content['search_term']} {sentence['keywords'][0]}"
+        result = resource.list(q=query, cx=CSE_KEY, searchType="image").execute()
+        print(f"> [images-collection] Querying Google Images with: {query}")
+        sentence["images"] = [item["link"] for item in result["items"]]
+        sentence["google_search_query"] = query
+
+        download_images(sentence["images"], i)
+
+
+def download_images(images: list, sentence_index: int):
+    downloaded_images = []
+    for i, url in enumerate(images):
+        response = requests.get(url)
+        try:
+            if response.status_code != 200:
+                raise Exception("Bad request in url of the image")
+            if url in downloaded_images:
+                raise Exception("Image already downloaded")
+
+            with open(f"data/images/{sentence_index}-org.png", "wb") as image:
+                image.write(response.content)
+                image.close()
+
+            downloaded_images.append(url)
+            print(
+                f"> [images-collection] [{sentence_index}][{i}] Image successfully downloaded: {url}"
+            )
+            break
+        except Exception as e:
+            print(f"> [images-collection] [{sentence_index}][{i}] Error ({url}): {e}")
+
